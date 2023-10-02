99b37c202f28efc0aa58d7b6d9ee98501cf0fe6c
Priyanshu-Nandy <priyanshunandy@gmail.com>
2023-10-02 07:13:24 +0530

    Animated website

---
+++ script.js
@@ -0,0 +1,56 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+function firstpageanim(){
+    var t1=gsap.timeline()
+    t1.from("#nav",{
+        y:'-10',
+        opacity:0,
+        duration:1.5,
+        ease:Expo.easeInOut
+    })
+    .to(".boundingelem",{
+        y:0,
+        ease:Expo.easeInOut,
+        duration:2,
+        delay:-1,
+        stagger:.2
+    })
+    .from("#herofooter",{
+        y:-10,
+        opacity:0,
+        duration:1.5,
+        delay:-1,
+        ease:Expo.easeInOut,
+    })
+
+}
+firstpageanim()
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff-40,
+        left: dets.clientX-30,
+        rotate: gsap.utils.clamp(-20, 20, diffrot),
+      });
+    });
+  });
+
+
